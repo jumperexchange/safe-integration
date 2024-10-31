e915a643f15ea6a18c0151b64b3bc57d39bc2321
Eugene Chybisov <imchybisov@gmail.com>
2024-10-31 16:13:40 +0100

    fix: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export function App() {
     }),
     [
       // getMultisigTransactionDetails,
-      // isDarkMode,
+      isDarkMode,
       // sendBatchTransaction,
       // shouldBatchTransactions,
     ]
