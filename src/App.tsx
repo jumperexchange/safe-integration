fd2c3d570e04bf8e911713cbb7374f26bed58346
Eugene Chybisov <imchybisov@gmail.com>
2024-11-01 14:44:58 +0100

    chore: bump widget

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,22 @@ import { lightTheme } from "./themes/light";
 
 export function App() {
   const { isDarkMode } = usePrefersColorScheme();
-  // const {
-  //   shouldBatchTransactions,
-  //   sendBatchTransaction,
-  //   getMultisigTransactionDetails,
-  // } = useSafeMultisig();
   const config = useMemo(
     (): Partial<WidgetConfig> => ({
       variant: "wide",
-      requiredUI: ["toAddress"],
+      // feeConfig: feeConfig,
       sdkConfig: {
-        // providers: [
-        //   EVM({
-        //     multisig: {
-        //       isMultisigWalletClient: true,
-        //       shouldBatchTransactions,
-        //       sendBatchTransaction,
-        //       getMultisigTransactionDetails,
-        //     },
-        //   }),
-        // ],
+        routeOptions: {
+          allowSwitchChain: false,
+        },
       },
       ...(isDarkMode ? darkTheme : lightTheme),
     }),
-    [
-      // getMultisigTransactionDetails,
-      isDarkMode,
-      // sendBatchTransaction,
-      // shouldBatchTransactions,
-    ]
+    [isDarkMode]
   );
   return (
     <main className="main">
-      <LiFiWidget integrator="fee-demo" config={config} />
+      <LiFiWidget integrator="safe-app" config={config} />
     </main>
   );
 }
